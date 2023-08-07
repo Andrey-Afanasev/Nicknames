16f1b98db4c3f73d575aa8f7badf237beb07446e
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-08-07 07:23:54 +0700

    7.08

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ class Validator {
     validateUsername(name) {
 
         const re = /[\w-]/.test(name) //латиница и цифры и символы тире -, подчёркивания _
-        const numbers = /[d{3,}]/.test(name) // 3 и более цифры
+        const numbers = /\d{3,}/.test(name) // 3 и более цифры
         const beginning = /^[^\d_-]/.test(name); // не начинается с цифры - и _
         const end = /[^\d_-]$/.test(name); // не заканчивается с цифры - и _
 
